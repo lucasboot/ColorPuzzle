5eddb4b8343a2174387d0e9d3e7cb077f69829fd
Lucas Figueredo <varela.alves@outlook.com>
2019-10-18 13:30:57 -0400

    Add files via upload

---
+++ joguis.py
@@ -0,0 +1,24 @@
+import pygame
+import time
+RED = (255, 0, 0)
+BLACK = (0,0,0)
+
+def setup():
+    pygame.init()
+    screen = pygame.display.set_mode([720, 720])
+    pygame.display.set_caption('Color Puzzle')
+    screen.fill([0, 0, 0])
+    pygame.display.flip()
+    time.sleep(5)
+
+
+class ball (id):
+    x = -50
+    y = -50
+    player = id
+    if (player ==)
+        color =
+    
+    pygame.draw.ellipse(screen, RED, [300, 200, 40, 40])
+     
+
